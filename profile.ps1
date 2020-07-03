278e6cb64ae9d33a694ff243e98c37c10935cba5
suidar <suidar@foxmail.com>
2020-07-03 17:37:13 +0800

    vscode and powershell settings

---
+++ profile.ps1
@@ -0,0 +1,11 @@
+New-Alias ~ $HOME
+
+New-Alias g git
+New-Alias ll ls
+New-Alias touch New-Item
+
+New-Alias vi D:\tools\Neovim\bin\nvim.exe
+New-Alias vim D:\tools\Neovim\bin\nvim.exe
+
+<# New-Alias cmake "C:\Program Files\CMake\bin\cmake.exe" #>
+
