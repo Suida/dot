c681b4cd3eef3f2eb1de2e8eb9111967ef9482ba
suida <suidar@foxmail.com>
2020-08-03 13:24:54 +0800

    Add vls config

--- project_files/ycm_extra_conf.py
+++ project_files/ycm_extra_conf.py
@@ -1,7 +1,48 @@
 def Settings(**kwargs):
     lang = kwargs['language']
+
     if lang == 'cfamily':
         return {
             'flags': ['-Wall', '-Wextra', '-Werror'],
         }
+
+    if lang == 'vue':
+        return {
+            'ls': {
+                'vetur': {
+                    'useWorkspaceDependencies': False,
+                    'validation': {
+                        'template': True,
+                        'style': True,
+                        'script': True,
+                    },
+                    'completion': {
+                        'autoImport': True,
+                        'useScaffoldSnippets': False,
+                        'tagCasing': 'initial',
+                        'scaffoldSnippetSources': {
+                            'workspace': '💼',
+                            'user': '🗒️',
+                            'vetur': '✌',
+                        },
+                    },
+                    'grammar': {'customBlocks': {}},
+                    'format': {
+                        'enable': True,
+                        'options': {'tabSize': 2, 'useTabs': False},
+                        'defaultFormatter': {
+                            'js': 'prettier',
+                            'ts': 'prettier',
+                        },
+                        'defaultFormatterOptions': {},
+                        'scriptInitialIndent': False,
+                        'styleInitialIndent': False,
+                    },
+                    'trace': {'server': 'off'},
+                    'dev': {'vlsPath': '', 'vlsPort': -1, 'logLevel': 'INFO'},
+                    'experimental': {'templateInterpolationService': False},
+                },
+            },
+        }
+
     return {}
