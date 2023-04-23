a970feac0acbde3d75e587f110588f3eb80aea2a
harass <suidar@foxmail.com>
2023-04-24 00:43:18 +0800

    :rotating_light: [NVIM] Fix deprecated warning, update format command

--- vim/lua/user/lspconfig.lua
+++ vim/lua/user/lspconfig.lua
@@ -31,7 +31,7 @@ local on_attach = function(_, bufnr)
   vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+  vim.keymap.set('n', '<leader>ft', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 local lsp_flags = {
