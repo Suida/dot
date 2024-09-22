a5fe49d76253ed35720c6d5a4d3b92d3f0ee6e6f
harass <suidar@foxmail.com>
2024-09-23 02:21:11 +0800

    :heavy_plus_sign: [NVIM] Update config of Trouble, add ui plugins

--- vim/lua/plugin_conf/plugins.lua
+++ vim/lua/plugin_conf/plugins.lua
@@ -139,15 +139,58 @@ require('lazy').setup({
     },
     'voldikss/vim-floaterm',
     'akinsho/toggleterm.nvim',
-    'willothy/flatten.nvim',
+    {
+      "willothy/flatten.nvim",
+      config = true,
+      -- or pass configuration with
+      -- opts = {  }
+      -- Ensure that it runs first to minimize delay when opening file from terminal
+      lazy = false,
+      priority = 1001,
+    },
     'tomtom/tcomment_vim',
+    {
+      'stevearc/dressing.nvim',
+      opts = {},
+    },
+    'rcarriga/nvim-notify',
     {
       'folke/trouble.nvim',
       dependencies = 'nvim-tree/nvim-web-devicons',
-      config = function()
-        require('trouble').setup {}
-        vim.keymap.set({ 'n' }, '<leader>tt', '<cmd>TroubleToggle<CR>')
-      end
+      opts = {},
+      cmd = "Trouble",
+      keys = {
+        {
+          '<leader>tt',
+          '<cmd>Trouble diagnostics toggle<cr>',
+          desc = 'Diagnostics (Trouble)'
+        },
+        {
+          '<leader>tT',
+          '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+          desc = 'Buffer Diagnostics (Trouble)',
+        },
+        {
+          '<leader>ts',
+          '<cmd>Trouble symbols toggle focus=false<cr>',
+          desc = 'Symbols (Trouble)',
+        },
+        {
+          '<leader>tl',
+          '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+          desc = 'LSP Definitions / references / ... (Trouble)',
+        },
+        {
+          '<leader>tL',
+          '<cmd>Trouble loclist toggle<cr>',
+          desc = 'Location List (Trouble)',
+        },
+        {
+          '<leader>tq',
+          '<cmd>Trouble qflist toggle<cr>',
+          desc = 'Quickfix List (Trouble)',
+        },
+      }
     },
     {
       'TimUntersberger/neogit',
