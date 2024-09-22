dfaffe67fa9ce19b87f376c66c5a88c26a6e1571
harass <suidar@foxmail.com>
2024-09-23 02:17:31 +0800

    :truck: [NVIM] Rename toggleterm's config file

--- vim/lua/plugin_conf/toggleterm.lua
+++ vim/lua/plugin_conf/toggleterm.lua
@@ -57,5 +57,5 @@ local lazygit = Terminal:new({
 function _lazygit_toggle()
   lazygit:toggle()
 end
-vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>gl", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
 
