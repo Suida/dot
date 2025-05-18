fa1cb50366e31bc1357403c053a5b944f27c35ff
harass <suidar@foxmail.com>
2025-05-18 15:02:12 +0800

    🚸 Optimize toggle behaviour of code companion

--- vim/lua/plugin_conf/codecompanion_ds.lua
+++ vim/lua/plugin_conf/codecompanion_ds.lua
@@ -2,6 +2,7 @@ local codecompanion_status_ok, codecompanion = pcall(require, 'codecompanion')
 if not codecompanion_status_ok then
   return
 end
+local context_utils = require("codecompanion.utils.context")
 
 codecompanion.setup({
   adapters = {
@@ -49,6 +50,7 @@ codecompanion.setup({
   display = {
     chat = {
       show_settings = true,
+      start_in_insert_mode = false,
 
       -- Change the default icons
       icons = {
@@ -66,11 +68,11 @@ codecompanion.setup({
 
       -- Options to customize the UI of the chat buffer
       window = {
-        layout = "float", -- float|vertical|horizontal|buffer
+        layout = "vertical", -- float|vertical|horizontal|buffer
         position = "right", -- left|right|top|bottom (nil will default depending on vim.opt.plitright|vim.opt.splitbelow)
         border = "single",
         height = 0.6,
-        width = 0.6,
+        width = 0.5,
         relative = "editor",
         full_height = true, -- when set to false, vsplit will be used to open the chat buffer vs. botright/topleft vsplit
         opts = {
@@ -80,6 +82,8 @@ codecompanion.setup({
           cursorcolumn = false,
           cursorline = false,
           foldcolumn = "0",
+          foldenable = true,
+          foldlevel = 1,
           linebreak = true,
           list = false,
           numberwidth = 1,
@@ -96,9 +100,53 @@ codecompanion.setup({
   },
 })
 
+local toggle = function()
+  local chat = codecompanion.last_chat()
+
+  if not chat then
+    return codecompanion.chat()
+  end
+
+  if chat.ui:is_visible() then
+    if vim.bo.filetype == 'codecompanion' then
+      return chat.ui:hide()
+    end
+  end
+
+  chat.context = context_utils.get(vim.api.nvim_get_current_buf())
+  codecompanion.close_last_chat()
+  chat.ui:open()
+  vim.api.nvim_command('startinsert!')
+end
+
+local close_codecompanion = function()
+  local chat = codecompanion.last_chat()
+
+  if not chat or not chat.ui:is_visible() then
+    return
+  end
+
+  chat.ui:hide()
+end
+
 require('plugin_conf.fidget-spiner'):init()
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>at', codecompanion.actions, opts)
-vim.keymap.set('n', '<leader>aa', codecompanion.toggle, opts)
-vim.keymap.set('n', '<leader>ac', codecompanion.chat, opts)
+vim.keymap.set('n', '<leader>an', codecompanion.chat, opts)
+vim.keymap.set('n', '<leader>ac', close_codecompanion, opts)
+vim.keymap.set({ 'n', 'i', 't' }, '<A-p>', toggle, opts)
+
+vim.api.nvim_create_autocmd({ "User" }, {
+  pattern = { "CodeCompanionChatHidden", "CodeCompanionChatClosed", },
+  callback = function(ev)
+    vim.api.nvim_input('jk')    -- Enter normal mode
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "User" }, {
+  pattern = { "CodeCompanionChatOpened" },
+  callback = function(ev)
+    vim.api.nvim_input('jk')    -- Enter insert mode
+  end,
+})
