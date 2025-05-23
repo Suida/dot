3aea213de055e9b7c6be4beea8d9dbe291a2005a
harass <suidar@foxmail.com>
2025-05-23 18:16:29 +0800

    ➕ Replace surround.vim with nvim-surround

--- vim/lua/plugin_conf/plugins.lua
+++ vim/lua/plugin_conf/plugins.lua
@@ -296,7 +296,28 @@ require('lazy').setup({
         require 'hop'.setup {}
       end
     },
-    'tpope/vim-surround',
+    {
+      "kylechui/nvim-surround",
+      version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+        require("nvim-surround").setup({
+          keymaps = {
+            insert = "<C-`>s",
+            insert_line = "<C-`>S",
+            normal = "ys",
+            normal_cur = "yss",
+            normal_line = "yS",
+            normal_cur_line = "ySS",
+            visual = "S",
+            visual_line = "gS",
+            delete = "ds",
+            change = "cs",
+            change_line = "cS",
+          },
+        })
+      end
+    },
     -- Brackets
     {
       'windwp/nvim-autopairs',
