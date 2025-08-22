e7ecb648b17854e91ac396358b9f7cd302ed8e04
harass <suidar@foxmail.com>
2025-08-22 10:50:29 +0800

    fix(nvim): Disable mason-lspconfig

--- vim/lua/plugin_conf/mason.lua
+++ vim/lua/plugin_conf/mason.lua
@@ -3,11 +3,5 @@ if not mason_status_ok then
   return
 end
 
-local mason_lsp_status_ok, mason_lsp = pcall(require, 'mason-lspconfig')
-if not mason_lsp_status_ok then
-  return
-end
-
 mason.setup{}
-mason_lsp.setup{}
 
