e94c765a1ea54ad710905ede82eed47545599c61
harass <suidar@foxmail.com>
2025-09-15 10:38:22 +0800

    ✨ Use deepseek-based avante AI

--- vim/lua/plugin_conf/avante_ds.lua
+++ vim/lua/plugin_conf/avante_ds.lua
@@ -6,37 +6,38 @@ end
 avante.setup {
   provider = 'deepseek',
   auto_suggestions_provider = 'deepseek',
-  cursor_applying_provider = 'groq',
   behaviour = {
     enable_cursor_planning_mode = true, -- enable cursor planning mode!
   },
   windows = {
     position = "right",
-    wrap = false, -- similar to vim.o.wrap
-    width = 40,
+    wrap = true, -- similar to vim.o.wrap
+    width = 45,
     ask = {
-      floating = false, -- Open the 'AvanteAsk' prompt in a floating window
-      start_insert = false, -- Start insert mode when opening the ask window
+      floating = true, -- Open the 'AvanteAsk' prompt in a floating window
+      start_insert = true, -- Start insert mode when opening the ask window
       border = "rounded",
       ---@type "ours" | "theirs"
       focus_on_apply = "ours", -- which diff to focus after applying
     },
   },
-  vendors = {
+  providers = {
     deepseek = {
       __inherited_from = "openai",
       api_key_name = "DEEPSEEK_API_KEY",
       endpoint = "https://api.deepseek.com",
-      model = "deepseek-coder",
+      model = "deepseek-chat",
       timeout = 30000,
     },
-    groq = { -- define groq provider
-      __inherited_from = 'openai',
-      api_key_name = 'GROQ_API_KEY',
-      endpoint = 'https://api.groq.com/openai/v1/',
-      model = 'llama-3.3-70b-versatile',
-      max_completion_tokens = 131072, -- remember to increase this value, otherwise it will stop generating halfway
-      timeout = 30000,
+  },
+  mappings = {
+    submit = {
+      normal = "<C-g>",
+      insert = "<C-g>",
+    },
+    cancel = {
+      normal = { "q" },
+      insert = {},
     },
   },
 }
--- vim/lua/plugin_conf/plugins.lua
+++ vim/lua/plugin_conf/plugins.lua
@@ -46,18 +46,6 @@ require('lazy').setup({
       event = "VeryLazy",
       lazy = false,
       version = false, -- set this if you want to always pull the latest change
-      opts = {
-        provider = "deepseek",
-        vendors = {
-          deepseek = {
-            __inherited_from = "openai",
-            api_key_name = "DEEPSEEK_API_KEY",
-            endpoint = "https://api.deepseek.com",
-            model = "deepseek-coder",
-            timeout = 30000,
-          },
-        },
-      },
       -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
       build = "make",
       -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
@@ -87,7 +75,6 @@ require('lazy').setup({
         },
         'MeanderingProgrammer/render-markdown.nvim',
       },
-      enabled = false,
     },
     {
       -- Make sure to set this up properly if you have lazy=true
